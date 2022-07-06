01c7443a03f2607274ce02c3fbc9c06659fcdf89
Zarina <91272567+introspkt@users.noreply.github.com>
2022-07-06 12:22:56 -0400

    Create survey.js

---
+++ survey.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
+
+//Questions to ask in order 
+What's your name? Nicknames are also acceptable :)
+What's an activity you like doing?
+What do you listen to while doing that?
+Which meal is your favourite (eg: dinner, brunch, etc.)
+What's your favourite thing to eat for that meal?
+Which sport is your absolute favourite?
+What is your superpower? In a few words, tell us what you are amazing at!
